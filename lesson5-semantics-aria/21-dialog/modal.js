fa73c60a36dbaf7a08ba8aa268a970b928773985
Fmressel <franzmichael@resselhaus.com>
2018-05-27 00:17:05 -0700

    Properly implement modal dialog

--- lesson5-semantics-aria/21-dialog/modal.js
+++ lesson5-semantics-aria/21-dialog/modal.js
@@ -63,7 +63,7 @@ function openModal(e) {
     }
   }
 
-  // FIXME: hide non-modal content from screen readers
+  document.getElementById('wrapper').setAttribute('aria-hidden', true);
 }
 
 function closeModal() {
@@ -74,5 +74,5 @@ function closeModal() {
   // Set focus back to element that had it before the modal was opened
   focusedElementBeforeModal.focus();
 
-  // FIXME: don't forget to make main content screen reader accessible again.
+  document.getElementById('wrapper').removeAttribute('aria-hidden');
 }
